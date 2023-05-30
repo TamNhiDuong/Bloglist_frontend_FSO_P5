688250de37ecb151b734b0ca79e890b159dbacdc
TamNhiDuong <yanxinyee@gmail.com>
2023-05-30 13:14:58 +0300

    Exc 5.17 e2e test: app shows login form by default

--- cypress/e2e/blog_app.cy.js
+++ cypress/e2e/blog_app.cy.js
@@ -1,9 +1,13 @@
 describe('Blog app e2e test', () => {
   beforeEach(function() {
+    cy.request('POST', 'http://localhost:3003/api/testing/reset')
     cy.visit('http://localhost:3000')
   })
 
   it('Login form is shown', function() {
-    // ...
+    cy.visit('http://localhost:3000')
+    cy.contains('username')
+    cy.contains('password')
+    cy.contains('login')
   })
 })
