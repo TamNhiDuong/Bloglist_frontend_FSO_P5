4b75dbe6c46a81f940b49a6f6ae11b45fe6f18d9
TamNhiDuong <yanxinyee@gmail.com>
2023-05-31 18:36:19 +0300

    Exc 5.18 Cypress e2e test login form

--- cypress/e2e/blog_app.cy.js
+++ cypress/e2e/blog_app.cy.js
@@ -1,6 +1,15 @@
+// npm run cypress:open
 describe('Blog app e2e test', () => {
   beforeEach(function() {
+    // Reset DB states
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    // Create a new user
+    const user = {
+      name: 'Tester',
+      username: 'testusername',
+      password: 'testpassword'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', user)
     cy.visit('http://localhost:3000')
   })
 
@@ -9,5 +18,21 @@ describe('Blog app e2e test', () => {
     cy.contains('username')
     cy.contains('password')
     cy.contains('login')
+  })
+
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('testusername')
+      cy.get('#password').type('testpassword')
+      cy.get('#login-button').click()
+      cy.contains('Tester logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type('testusername')
+      cy.get('#password').type('testpass')
+      cy.get('#login-button').click()
+      cy.contains('Wrong credentials')
+    })
   })
 })
